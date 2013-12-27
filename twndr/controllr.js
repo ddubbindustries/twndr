ae73274546739c5a4c6549d22cfc994270721b10
ddubbindustries <darkestofwings@gmail.com>
2013-12-27 15:41:54 +0000

    process data with local storage

--- twndr/controllr.js
+++ twndr/controllr.js
@@ -25,7 +25,6 @@ var getBestScore = function(lines, wordsPerLine, scoreMin){
   return getScore(best.txt, true)+' / '+k+' tries / '+ best.txt.length+' chars';
 };
 
-
 $(document).ready(function(){
  
  var cfg = {
@@ -44,7 +43,7 @@ $(document).ready(function(){
     $output = $('#output');
   
   $refresh = $('<button/>').html('Refresh').click(function(){
-    var composition = util.getConfigs(); 
+    var cfg = util.getConfigs(); 
     $output.html(lex.output.format(cfg));
   });
 
@@ -56,27 +55,64 @@ $(document).ready(function(){
 
   // data: {count: 100, geocode: '37.22,-80.42,15mi', lang: 'en'},
   lex.init();
-  
-  $.ajax({
-    url: 'http://p.ddubb.net/db/', 
-    data: {table: 'tweets', limit: 5000},
-    dataType: 'jsonp',
-    success: function(data){
-      var input = '';
-      $.each(data.results, function(k,v){
-        var chunk = JSON.parse(v.data);
-        if (chunk.retweet_count > 0) return true;
-        //input += (chunk.created_at + ': ' + chunk.text+"\n\n");
-        lex.addChunk(chunk.text);
-      });
-      //$input.append(input);
-      lex.sortTop(cfg.topCount);
-      console.log('meta', lex.meta);
-      dump(lex.meta.top.map(function(v){return v.word+' '+v.count;}));
-      $refresh.click();
-    }
-  });
-    
+
+var rep = 0, repMax = 10;
+var go = {
+  getApi: function(time) {
+    if (rep++ == repMax) return go.finalize();
+    $.ajax({
+      url: 'http://p.ddubb.net/db/', 
+      data: {table: 'tweets', time: time+',', limit: 3000},
+      dataType: 'jsonp',
+      cache: true,
+      success: function(data){
+        var input = '', nextTime = 0, dupes = 0;
+        $.each(data.results, function(k,v){
+          var chunk = JSON.parse(v.data);
+          if (lex.chunks[chunk.id_str]) return console.log('duplicates');
+        
+          chunk = util.simplifyTweetObj(chunk);
+
+          input += (chunk.created_at + ': ' + chunk.text+"\n\n");
+          lex.chunks[chunk.id_str] = chunk;
+          lex.addChunk(chunk.text);
+          nextTime = v.time;
+        });
+        $input.append(input+"====================\n\n\n");
+        util.local.store('chunks', lex.chunks);
+        
+        lex.sortTop(cfg.topCount);
+        console.log(rep, 'of', repMax, 'meta', lex.meta);
+        dump(lex.meta.top.map(function(v){return v.word+' '+v.count;}));
+        $refresh.click();
+        go.getApi(nextTime);
+      }
+    });
+  },
+  getStored: function(){
+    var retweets = 0, replies = 0, input = '';
+    var chunks = util.local.get('chunks');
+    $.each(chunks, function(k, chunk){
+      if (chunk.retweet_count > 0) return retweets++;
+      if (chunk.in_reply_to_user_id_str) return replies++;
+      lex.chunks[chunk.id_str] = chunk;
+      lex.addChunk(chunk.text);
+      input += (chunk.created_at + ': ' + chunk.text+"\n\n");
+    });
+    $input.append(input);
+    lex.sortTop(cfg.topCount);
+    console.log('retweets', retweets, 'replies', replies);
+    console.log('meta', lex.meta);
+    dump(lex.meta.top.map(function(v){return v.word+' '+v.count;}));
+    $refresh.click();
+  },
+  finalize: function(){
+    console.log('max reps');
+  }
+};
+//go.getApi();
+go.getStored();
+
 });   
 
 
