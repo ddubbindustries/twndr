6090c4aaad220a2eba681fda05205ec12b26faa3
ddubbindustries <darkestofwings@gmail.com>
2013-12-27 16:45:06 +0000

    configs saved in localStorage

--- twndr/controllr.js
+++ twndr/controllr.js
@@ -1,68 +1,49 @@
-var getScore = function(string, markup){
-  var score = 0, words = string.split(' '), scoredWords = [], out = '';
-  $.each(words, function(k,v){
-    if (lex.isInArr(lex.meta.top, 'word', v) && scoredWords.indexOf(v) == -1) {
-      scoredWords.push(v);
-      score++;// += lex.list[v].count;
-      //v += '<sup>'+lex.list[v].count+'</sup>';
-      out += '<span class="hilight">'+v+'</span> ';
-    } else {
-      out += v + ' ';
-    }
-  });
-  //score = (score / lex.cfg.topCount).toFixed(2);
-  return markup ? out + '<br/>' + score : score;
-};
-var getBestScore = function(lines, wordsPerLine, scoreMin){
-  var temp = {txt: '', score:0}, best = {txt:'', score:0};
-  for(k=0; k<lex.cfg.bestScoreMaxTries; k++) {
-    temp.txt = generateLines(lines, wordsPerLine);
-    temp.score = getScore(temp.txt);
-    if (temp.score > best.score) {
-      best.score = temp.score; best.txt = temp.txt;
-    }
-  }
-  return getScore(best.txt, true)+' / '+k+' tries / '+ best.txt.length+' chars';
-};
-
-$(document).ready(function(){
- 
- var cfg = {
-      sets: 5, 
-      lines: 1, 
-      minWords: 2, 
-      maxWords: 20,
-      maxChars: 115,
-      minScore: 2,
-      lineEnd: '',
-      stats: true,
-      optimize: true,
-      topCount: 15
-    },
-    $input = $('#input'),
-    $output = $('#output');
-  
-  $refresh = $('<button/>').html('Refresh').click(function(){
-    var cfg = util.getConfigs(); 
-    $output.html(lex.output.format(cfg));
-  });
-
-  util.buildConfigs(cfg, function(){ 
-    $refresh.click();
-  });
-  
-  $refresh.appendTo('#configs');
-
   // data: {count: 100, geocode: '37.22,-80.42,15mi', lang: 'en'},
-  lex.init();
+ 
+var cfg = util.local.get('cfg') || {
+    sets: 5, 
+    lines: 1, 
+    minWords: 2, 
+    maxWords: 20,
+    maxChars: 115,
+    minScore: 2,
+    lineEnd: '',
+    stats: true,
+    optimize: true,
+    topCount: 15
+  },
+  $input,
+  $output,
+  rep = 0, 
+  repMax = 10;
 
-var rep = 0, repMax = 10;
 var go = {
-  getApi: function(time) {
+  init: function(){ 
+    lex.init();
+  
+    $input = $('#input');
+    $output = $('#output');
+    
+    $refresh = $('<button/>').html('Refresh').click(function(){
+      var cfg = util.getConfigs();
+      lex.sortTop(cfg.topCount);
+      console.log('meta', lex.meta);
+      dump(lex.meta.top.map(function(v){return v.word+' '+v.count;}));
+      $output.html(lex.output.format(cfg));
+    });
+
+    util.buildConfigs(cfg, function(){
+      util.local.store('cfg', util.getConfigs()); 
+      $refresh.click();
+    });
+    
+    $refresh.appendTo('#configs');
+  },
+  getApi: function(startTime) {
     if (rep++ == repMax) return go.finalize();
     $.ajax({
       url: 'http://p.ddubb.net/db/', 
-      data: {table: 'tweets', time: time+',', limit: 3000},
+      data: {table: 'tweets', time: startTime+',', limit: 3000},
       dataType: 'jsonp',
       cache: true,
       success: function(data){
@@ -81,9 +62,6 @@ var go = {
         $input.append(input+"====================\n\n\n");
         util.local.store('chunks', lex.chunks);
         
-        lex.sortTop(cfg.topCount);
-        console.log(rep, 'of', repMax, 'meta', lex.meta);
-        dump(lex.meta.top.map(function(v){return v.word+' '+v.count;}));
         $refresh.click();
         go.getApi(nextTime);
       }
@@ -102,17 +80,19 @@ var go = {
     $input.append(input);
     lex.sortTop(cfg.topCount);
     console.log('retweets', retweets, 'replies', replies);
-    console.log('meta', lex.meta);
-    dump(lex.meta.top.map(function(v){return v.word+' '+v.count;}));
     $refresh.click();
   },
   finalize: function(){
     console.log('max reps');
   }
 };
-//go.getApi();
-go.getStored();
-
+$(document).ready(function(){
+  go.init();
+  if (util.local.get('chunks')) {
+    go.getStored();
+  } else {
+    go.getApi();
+  }
 });   
 
 
