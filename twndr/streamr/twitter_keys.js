532f130287039d767eadb5a7ec2e3d980b1694eb
ddubbindustries <darkestofwings@gmail.com>
2013-12-30 05:38:31 +0000

    removed twitter keys from repo

--- twndr/streamr/twitter_keys.js
+++
@@ -1,6 +0,0 @@
-exports.keys = {
-  consumerKey:	'B62QrvyMSVpkuHqkqYZhA',
-  consumerSecret:	'RvrwYeC1N0O4WdNcOh2cfJuYmUShBpfayHmYPK2hT6M',
-  token: 			'815249522-lWoGCr9DBzuRQL11iSsbC7v9QF8XdDlwgYiX1kBd',
-  secret: 		'VGjP5MvktSOjrVwHVCxbEmCZr0j8V8Exsgj9rDAOqk'
-};
