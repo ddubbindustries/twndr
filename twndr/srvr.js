61dc4504ed6ffcf303ae9b4c047a8d7e225968d4
ddubbindustries <darkestofwings@gmail.com>
2014-01-12 07:37:19 +0000

    able to reinit after a tweet

--- twndr/srvr.js
+++ twndr/srvr.js
@@ -1,35 +1,53 @@
-var cfg = require('./cfg.js').cfg,
-    go = require('./controllr.js').go,
-    tweetr = require('./tweetr'),
-    emailr = require('./emailr');
+cfg = require('./cfg.js').cfg;
+util = require('../lib/util.js').util;
+go = require('./controllr.js').go;
+tweetr = require('./tweetr');
+emailr = require('./emailr');
 
-cfg.maxTries = 1000;
-cfg.maxTime = 3000;
-cfg.streamInterval = 60*10;
+cfg.maxTries = 2000;
+cfg.maxTime = 10000;
+cfg.maxChunks = 2000;
+cfg.streamInterval = 60*15;
+cfg.postTweet = false;
+cfg.stats = false;
 
-go.init({
+var hooks = {
   init: function(){
     console.log('server init', cfg);
   },
   process: function(chunks){},
-  refresh: function(){ 
+  refresh: function(lex){ 
     console.log({refresh: new Date().toISOString(), chunkCount: lex.meta.chunkCount, tallyCount: lex.meta.tallyCount});
     console.log('chunks:', go.meta);
-    console.log('top:', lex.meta.topArr.slice(0, cfg.topCount).join(', '));
-    console.log('twip:', lex.output.format(cfg));
+    console.log('top:', lex.meta.topArr.join(', '));
+    var twipObj = lex.output.line(cfg);
+    console.log('twip:', twipObj.text, twipObj.stats);
   },
-  finalize: function(){
+  finalize: function(lex){
     var tweet = lex.output.format(cfg);
 		console.log('ready to tweet:', tweet);
-    tweetr.postTweet(tweet);
+    if (cfg.postTweet) tweetr.post(tweet);
+    
+    var altCfg = cfg;
+        altCfg.sets = 5;
+        altCfg.stats = true;
+    
     emailr.send({
       from: 'Twndr <bot@twndr.com>',
       to: 'dtw@vt.edu',
       subject: tweet,
-      text: JSON.stringify(lex.meta.topArr,null,'\t') +
+      text: JSON.stringify({
+          cfg: cfg,
+          alt: lex.output.format(altCfg),
+          topArr: lex.meta.topArr
+        }, null, '\t') +
         "\n\n@twndr via Mailgun"
     });
-    go.init();
+    util.local.store(new Date().toISOString().replace(/[^0-9]/g,'') + '_chunks', lex.chunks);
+    util.local.store('chunks','');
+    go.init(hooks);
   }
-});
+};
+
+go.init(hooks);
 
