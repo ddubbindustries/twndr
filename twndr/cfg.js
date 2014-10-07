e392f477bcb4eeca6fa47e2d609ee988b87c32bb
ddubbindustries <darkestofwings@gmail.com>
2014-10-07 23:48:37 +0000

    moving things around a bit

--- twndr/cfg.js
+++ twndr/cfg.js
@@ -7,15 +7,18 @@ var cfg = {
   maxTries: 500,
   maxTime: 2000,
   lineEnd: '',
-  topCount: 15,
+  topCount: 30,
   stats: false,
   optimize: true,
-  
-  maxChunks: 2000,
+  maxChunks: 300,
   //timeStart: 'Tue Aug 13 22:24:22 +0000 2013',
   //timeEnd: 'Fri Aug 16 13:18:44 +0000 2013',
+  geo: 'bbg',
   stream: true,
-  streamInterval: 30
+  streamInterval: 60 * 10,
+  filterWords: ['#job', '#jobs', '#tweetmyjobs'],
+  useLocalStore: false,
+  postTweet: true
 };
 
 if (typeof exports !== 'undefined') exports.cfg = cfg;
