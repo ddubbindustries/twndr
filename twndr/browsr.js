db6298497518bb44e559a048111897c1df6580f0
ddubbindustries <darkestofwings@gmail.com>
2015-10-02 03:58:06 +0000

    MAJOR REFACTORING: 1) search any location with new API (back only) 2) use twitter lib 3) respond via @twndr 4) much more

--- twndr/browsr.js
+++ twndr/browsr.js
@@ -1,45 +1,51 @@
-var hooks = {
-  init: function(){
-    cfg = util.local.get('cfg') || cfg;
-    console.log('client init', cfg);
-    $input = $('#input');
-    $output = $('#output');
-    $refresh = $('<button/>').html('Refresh').click(go.refresh);
-    $rebuild = $('<button/>').html('Rebuild').click(go.init);
-    $stop = $('<button/>').html('Stop').click(go.stopStream);
-    $start = $('<button/>').html('Start').click(go.startStream);
-    $filter = $('<button/>').html('Filter').click(go.filterChunks);
+var initBrowser = function(){
+  var cfg = util.local.get('cfg') || {};
+  $('#search').val(cfg.search);
+  $('#input').submit(function(e){
+    e.preventDefault();
+    $('#twend, .col').empty();
+    cfg.search = $('#search').val();
+    util.local.store('cfg', cfg);
+    var Twndr = new Go({
+      search: cfg.search,
+      afterBatch: function(out){
 
-    util.buildConfigs(cfg, function(){
-      util.local.store('cfg', util.getConfigs()); 
-      go.refresh();
+        $('#out').append(out.text);
+
+        $('#twend').text(out.twend);
+        $('#meta').empty();
+        $.each(out.topArr, function(i,v){
+          var idselectors = '#'+v.ids.join(', #');
+          $('<li>'+v.word+' '+v.count+'</li>').click(function(){
+            if ($(this).is('.hilight')) {
+              $('.tweet').show();
+              $('#meta .hilight').removeClass('hilight');
+            } else {
+              $('#meta .hilight').removeClass('hilight');
+              $(this).addClass('hilight'); 
+              $('.text .hilight').contents().unwrap();
+              $('.tweet').hide().filter(idselectors).show().find('.text').html(function(i,html){
+                return html.replace(new RegExp('\\b'+v.word+'\\b', 'ig'), '<span class="hilight">$&</span>');
+              });
+            }
+          }).appendTo('#meta');
+        });
+        $('<li>[ALL]</li>').hover(function(){$('#out li').show();}).appendTo('#meta');
+      },
+      afterAll: function(go) {
+        $('.tweet').click(function(){
+          console.log(go.tweetStore.ok[$(this).attr('id')]);
+        }); 
+        console.log('all done, total tweets:', Object.keys(go.tweetStore.ok).length, go.twend);
+      }
     });
-    
-    $('#configs').append($refresh, $rebuild, $stop, $start, $filter);
-  },
-  process: function(chunk){
-    var out = chunk.created_at + ': ' + chunk.text + ' | ' + 
-      (chunk.place ? chunk.place.full_name : '???');
-    var $p = $('<p/>').html(out)
-      .data(chunk)
-      .click(function(){
-        console.log('data', $(this).data());
-      });
-    $input.prepend($p);
-  },
-  refresh: function(){ 
-    cfg = util.local.get('cfg') || cfg;
-    console.log('refresh cfg', go.cfg);
-    console.log('meta', lex.meta, "\nlatest", go.meta);
-    dump(lex.meta.topArr);
-    $output.html(lex.output.format(cfg));
-  },
-  finalize: function(){
-    console.log('final hook');
-  }
+  }).submit();
+  
+  $('#cache').click(function(){
+    localStorage.clear();
+    $('#input').submit();
+  });
 };
 
-$(document).ready(function(){
-  go.init(hooks);
-});
+$(document).ready(initBrowser);
 
---
+++ twndr/proxy.js
@@ -0,0 +1,46 @@
+var http = require('http'),
+    url = require('url'),
+    keys = require('./twitter_keys').dev_keys,
+    port = 8080,
+    req,
+    res;
+
+var Twitter = new require('twitter'),
+  client = new Twitter({
+    consumer_key: keys.consumerKey,
+    consumer_secret: keys.consumerSecret,
+    access_token_key: keys.token,
+    access_token_secret: keys.secret
+  });
+
+//,geocode:"37.2300,80.4178,3mi
+// http://jsbin.com/yusosumale/1/edit?js,output
+
+http.createServer(function(req, res) {
+	
+  console.log('request:', new Date().toISOString(), req.url);
+  
+  if (req.headers.host !== 'p.ddubb.net:'+port || req.url.indexOf('/favicon.ico') > -1) {
+    res.writeHead(404)
+    res.end();
+    return false;
+  }
+  
+  var urlObj = url.parse(req.url, true),
+    path = urlObj.pathname.slice(1),
+    params = urlObj.query,
+    callback = params.callback;
+  
+  delete params.callback;
+
+  client.get(path, params, function(err, data, response){
+    if (err) data = err;
+    if (data.statuses) console.log('response tweets', data.statuses.length);
+    data = JSON.stringify(data);
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(callback ? callback + '(' + data + ');' : data);
+  });
+
+}).listen(port);
+
+console.log('srvr up on', port);
